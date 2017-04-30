7c161dc1c2b58461ddc1dcbb6499cbdc30cb7984
Alejandra Fernandez <alexftelle@gmail.com>
2017-04-30 11:07:46 +0200

    Create saas-project-app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SaasProjectApp::Application.config.secret_key_base = '1170e97a54485bbf4d936e89a8e24c543880025ad05e4c51d0dc5e7dd2b839b272b6a372a2ca51b11e749e65fda5b6dc5e356fa8a9093a27b72597314a2513f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SaasProjectApp::Application.config.session_store :cookie_store, key: '_saas-project-app_session'
